e5f9b7177fff853012552d876b6944765dc25c76
Vardan Hovhannisyan <30779754+var2dan@users.noreply.github.com>
2017-12-15 19:30:33 +0400

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!!')
 })
 
+app.get('/healthcheck', function (req, res) {
+  res.send('The server is up')
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
